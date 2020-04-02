e651e09c6d19cc14a67aa8d4a7901b491e5740c3
Niculescu Teodor-Vicentiu <teodorvicentiuniculescu@gmail.com>
2020-04-02 20:25:11 +0300

    Changed test names and wrote the test for init cont

--- MicroprocessorSimulator.py
+++ MicroprocessorSimulator.py
@@ -5,17 +5,22 @@ TEST_FOLDER_PATH: str = "tests/"
 TEST_FILE_TYPE: str = ".test"
 RESULT_FILE_TYPE: str = ".result"
 
-TEST1: str = "test1"
-TEST_ERRORS1: str = "test_errors1"
-TEST_ERRORS2: str = "test_errors2"
+TEST_LIST: list = [
+    "2_and_gates",
+    "insert_block_errors",
+    "insert_edge_errors",
+    "insert_init_cond_errors"
+]
 
 
 def main():
-    file_path: str = TEST_FOLDER_PATH
-    file_path += TEST_ERRORS2
-    file_in = file_path + TEST_FILE_TYPE
-    file_out = file_path + RESULT_FILE_TYPE
-    FileInterpreter(file_in, file_out, Simulation()).parse()
+    for test_path in TEST_LIST:
+        # Run every test in the "tests" folder
+        file_path: str = TEST_FOLDER_PATH
+        file_path += test_path
+        file_in = file_path + TEST_FILE_TYPE
+        file_out = file_path + RESULT_FILE_TYPE
+        FileInterpreter(file_in, file_out, Simulation()).parse()
 
 
 if __name__ == "__main__":
