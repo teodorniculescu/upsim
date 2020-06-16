2d980d8e3e96eb6cde493cf61b0cf7961fe5f1f0
Teodor Niculescu <teodorniculescu@yahoo.com>
2020-06-16 15:21:24 +0300

    Created Buffer class in the Buffer.py file

--- blocks/store/Buffer.py
+++ blocks/store/Buffer.py
@@ -22,3 +22,20 @@ class DIGITAL_TRI_STATE_BUFFER(LogicalBlock):
                 output_pin.set_low()
         else:
             output_pin.set_high_impedance()
+
+
+class BUFFER(LogicalBlock):
+    def __init__(self, block_name: str):
+        super().__init__(block_name)
+        super().add_pin(BaseValue("in", PIN_TYPE_INPUT))
+        super().add_pin(BaseValue("out", PIN_TYPE_OUTPUT))
+
+    def calculate(self) -> None:
+        input_pin: BaseValue
+        output_pin: BaseValue
+        [input_pin] = self.get_all_pins_with_type(PIN_TYPE_INPUT).values()
+        [output_pin] = self.get_all_pins_with_type(PIN_TYPE_OUTPUT).values()
+        if input_pin.is_high():
+            output_pin.set_high()
+        elif input_pin.is_low():
+            output_pin.set_low()
